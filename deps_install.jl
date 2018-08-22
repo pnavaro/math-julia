b42dce166d20cc91286adafa75a43fa63a44ab62
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-08-22 18:08:45 +0200

    Add Parallel computation notebook

--- deps_install.jl
+++ deps_install.jl
@@ -30,10 +30,13 @@ end
    open("runtests.jl", "w") do f
    
       write(f,  header)
+      write(f,"@testset \"notebooks\" begin\n"
    
       for nbfile in nbfiles
          write(f, "@test testnb(\"$nbfile\")\n")
       end
+      
+      write(f,"end;")
    
    end
 
