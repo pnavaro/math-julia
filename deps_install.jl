b96ee26c49cf8e2d0a2fd58a3248364eb425ffdd
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-08-22 15:05:16 +0200

    Add travis CI configuration

--- deps_install.jl
+++ deps_install.jl
@@ -1,6 +1,42 @@
-using Pkg
-io = open("REQUIRE", "r")
-deps = read(io, String)
-for dep in split(deps)
-   Pkg.add(String(dep))
+using Pkg, Glob
+
+function install_deps()
+   io = open("REQUIRE", "r")
+   deps = read(io, String)
+   
+   for dep in split(deps)
+      Pkg.add(String(dep))
+   end
 end
+
+function write_tests()
+   nbfiles = glob("*.ipynb")
+   
+   header = """
+using Test, NBInclude
+
+function testnb(nbfile::String)
+   try
+       @nbinclude(nbfile)
+       true
+   catch
+       false
+   end
+end
+
+"""
+
+   open("runtests.jl", "w") do f
+   
+      write(f,  header)
+   
+      for nbfile in nbfiles
+         write(f, "@test testnb(\"$nbfile\")\n")
+      end
+   
+   end
+
+end
+
+install_deps()
+write_tests()
